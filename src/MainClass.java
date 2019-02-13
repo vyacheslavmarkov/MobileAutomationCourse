9817c993b4bbcabeebc5517fc0b26239ee7d82dc
vmarkov <vmarkov@weezlabs.com>
2019-02-13 13:12:08 +0300

    Lesson 0, test 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,9 @@
 public class MainClass {
+    private int class_number = 20;
+
+    public int getClassNumber() {
+        return class_number;
+    }
 
     public int getLocalNumber() {
         return 14;
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 public class MainClassTest {
     private MainClass mainClass = new MainClass();
 
+    // Test 1
     @Test
     public void testGetLocalNumber() {
         /* I decided not to add particular values of comparing numbers to the error message,
@@ -11,5 +12,13 @@ public class MainClassTest {
          */
         Assertions.assertEquals(mainClass.getLocalNumber(), 14,
                 "getLocalNumber function returned unexpected number!");
+    }
+
+    // Test 2
+    @Test
+    public void testGetClassNumber() {
+        Assertions.assertTrue(mainClass.getClassNumber() > 45,
+                "getClassNumber returned value that is smaller than 45!" +
+                        "\nThe value is " + mainClass.getClassNumber());
     }
 }
